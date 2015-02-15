bf5ffc2958781835ebf87fc94b9c2f0098386328
tad <terry.donaghe@gmail.com>
2015-02-15 14:35:56 -0800

    Cleaned up extraneous code

--- history-page-snippet.js
+++ history-page-snippet.js
@@ -6,12 +6,6 @@ $(document).ajaxComplete(function(){
     var command = $(".dd-title.d-dialog-title").first().text();
     if(lastCommand != command){
         $.get('http://localhost:8182/echocommand?q=' + command);
-        if(command === 'alexa turn all the lights off') {
-            $.get('http://localhost:8182/turnalloff');
-        }
-        if(command === 'alexa turn all the lights on') {
-            $.get('http://localhost:8182/turnallon');
-        }
         lastCommand = command;
     }
 });
